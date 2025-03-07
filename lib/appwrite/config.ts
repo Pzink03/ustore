973fc211522b0d6a82e93bd8dadfcf88b982b550
Pete <pete.zink03@gmail.com>
2025-03-06 20:40:42 -0500

    Can now create users in the appwrite database. Creation of a user will send them an OTP verification email coming from appwrite.

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,9 +1,9 @@
 export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
-  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DB!,
-  usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_USERS!,
-  filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_FILES!,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+  usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
   bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
   secretKey: process.env.NEXT_APPWRITE_KEY!,
 }
